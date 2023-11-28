5f634542232adc55201ff9c6dc223437b7c5c5ef
Chris-Effah <akboateng41@gmail.com>
2023-11-27 17:37:12 -0800

    fixed formatting errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -45,7 +45,7 @@ def get_employee_todo_progress(employee_id):
 
         for task in todo_list:
             if task['completed']:
-                print(f"\t {task['title']}")
+                print(f"\t- {task['title']}")
 
 
 if __name__ == "__main__":
