1c82f7c0ead73005f0f2babacb0cd4a069c2ebab
Chris-Effah <akboateng41@gmail.com>
2023-12-12 11:24:50 -0800

    trial

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,6 +6,9 @@ a script that returns the number of subscribers
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    a function that returns the number of subscribers
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "Mozilla/5.0"
