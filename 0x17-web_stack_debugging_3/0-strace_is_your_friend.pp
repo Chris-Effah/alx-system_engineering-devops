62923c43bd3e29e97762f061110049c835d3623a
Chris-Effah <akboateng41@gmail.com>
2023-12-13 13:25:36 -0800

    pycodestlye

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# A puppet script that fixes a webstack error
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
